cf80d15fb93817b54ec67b94c33f26dbb4250138
Gauri <gauripatil2999@gmail.com>
2025-08-01 17:10:41 +0530

    Project 55: Personal Budget Planner

---
+++ part-07/07_python-adv1.py
@@ -0,0 +1,90 @@
+""" Project: Personal Budget Planner """
+
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+expenses = []
+
+def set_saving_goal():
+    goal = float(input("Enter your  monthly saving goal: "))
+    print(f'Your saving goal is set to ${goal:.2f}')
+    return goal
+
+def add_income():
+    income = float(input("Enter your income amount: "))
+    print(f"Income of ${income:.2f} added.")
+    return income
+
+def add_expense():
+    category = input("Enter expense category (eg, Food, Travel,...): ").capitalize()
+    amount = float(input("Enter the expense amount: "))
+    expenses.append({'category': category, 'amount': amount})
+    print(f"Expense of ${amount:.2f} added under {category}")
+
+def view_expenses_by_category():
+    category_totals = defaultdict(float)
+    for expense in expenses:
+        category_totals[expense['category']] += expense['amount']
+    print("\nExpenses by category: ")
+    for category, total in category_totals.items():
+        print(f"{category}: ${total:.2f}")
+
+def calculate_remaining_budget(income, expenses):
+    total_expenses = sum(expense['amount'] for expense in expenses)
+    remaining = income - total_expenses
+    print(f"Total expenses: ${total_expenses:.2f}")
+    print(f"Remaining budget: ${remaining:.2f}")
+    return remaining
+
+def check_saving_goals(remaining, goal):
+    if remaining >= goal:
+        print(f"Congratulations! You have met your saving goal with ${remaining - goal} extra.")
+    else:
+        print(f"You are ${goal - remaining:.2f} away from your savings goal.")
+
+def plot_expenses():
+    category_totals = defaultdict(float)
+    for expense in expenses:
+        category_totals[expense['category']] += expense['amount']
+
+    labels = category_totals.keys()
+    sizes = category_totals.values()
+    plt.pie(sizes, labels= labels, autopct= "%1.1f%%")
+    plt.title("Expense Distribution")
+    plt.show()
+
+def main():
+    print("\n----Welcome to the personal budget planner!----")
+    goal = set_saving_goal()
+    income = add_income()
+
+    while True:
+        print("\nOptions")
+        print("1. Add Expense")
+        print("2. View expenses by category")
+        print("3. Calculate remaining budget")
+        print("4. Check saving goal")
+        print("5. Visualize expenses")
+        print("6. Exit")
+        
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add_expense()
+        elif choice == "2":
+            view_expenses_by_category()
+        elif choice == "3":
+            calculate_remaining_budget(income, expenses)
+        elif choice == "4":
+            remaining = calculate_remaining_budget(income, expenses)
+            check_saving_goals(remaining, goal)
+        elif choice == "5":
+            plot_expenses()
+        elif choice == "6":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again!")
+
+if __name__ == "__main__":
+    main()
